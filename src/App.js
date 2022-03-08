fcf28a5ebac442eb1901873028cb663464d587a7
KINGSLEYEHAPA <kessity09@gmail.com>
2022-03-08 20:19:59 +0100

    Starting my portfolio website

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 function App() {
-  return <div className="App">This is App Component</div>;
+  return (
+    <div className="text-orange-500 font-bont text-center text-4xl p-4 ">
+      This is App Component
+    </div>
+  );
 }
 
 export default App;
