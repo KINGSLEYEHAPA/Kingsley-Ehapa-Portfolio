1d166962fbc2fea2288b2dcf953ad0f989e6f6ff
KINGSLEYEHAPA <kessity09@gmail.com>
2022-03-11 19:13:56 +0100

    Removing Redux Setup in the app

--- src/components/AppRoutes.jsx
+++ src/components/AppRoutes.jsx
@@ -1,22 +1,18 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "../App";
-import { Provider } from "react-redux";
-import store from "../redux/store";
 import Homepage from "./Homepage";
 
 const AppRoutes = () => {
   return (
     <>
-      <Provider store={store}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<App />}>
-              <Route index element={<Homepage />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </Provider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route index element={<Homepage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
--- src/redux/store.js
+++
@@ -1,6 +0,0 @@
-import { createStore } from "redux";
-import myPortfolioReducer from "./reducer/myPortfolioReducer";
-
-const store = createStore(myPortfolioReducer);
-
-export default store;
