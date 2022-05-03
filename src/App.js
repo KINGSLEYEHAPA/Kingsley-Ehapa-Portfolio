7ea3a87cd0101c338e6dd07582ead3382a07a0b0
KINGSLEYEHAPA <kessity09@gmail.com>
2022-05-03 15:32:00 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -6,6 +6,19 @@ import "./assets/background.jfif";
 function App() {
   return (
     <div className="m-0 p-0  font-sans  max-w-7xl mx-auto bg-slate-700">
+      {/* <div
+        className="max-w-7xl"
+        style={{
+          clipPath: "polygon(0 0, 100% 0, 100% 60%, 0 60%)",
+          position: "absolute",
+          width: "100%",
+          minHeight: "100%",
+          top: "0",
+          left: "0",
+          zIndex: "-2",
+          backgroundColor: "rgb(51 65 85)",
+        }}
+      ></div> */}
       <Header />
       <Outlet />
       <Footer />
